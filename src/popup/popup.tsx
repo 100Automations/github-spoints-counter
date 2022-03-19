56dafb6c02553b20ee37eefeaef8aeaed950ed8b
avali4907 <avali4907@outlook.com>
2022-03-18 17:30:30 -0700

    added preact to compile

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,86 +1,53 @@
 // @ts-nocheck
+"use strict";
+
 import './popup.css';
 
 
-/*
+console.log(document.getElementById('app'))
 
+const App = (
+  <div className="app">
     <div id="popup" class="container p-3">
-      <form class="row align-items-center">
-        <div class="col-3">
-          <button class="btn btn-primary btn-sm" type="button">Edit</button>
-        </div>
-        <div class="col-6">
-          <input class="form-control" type="text" value="size" disabled />
-        </div>
-        <div class="col-3 form-switch d-flex justify-content-end">
-          <input class="form-check-input" type="checkbox" />
-        </div>
-      </form>
-      <form class="row align-items-center">
-        <div class="col-3">
-          <button class="btn btn-primary btn-sm" type="button">Submit</button>
-        </div>
-        <div class="col-6">
-          <input class="form-control" type="text" value="size" />
-        </div>
-      </form>
-      <div>
-        <button type="button" class="btn btn-primary btn-sm">Add Filter</button>
-      </div>
+      <Series />
     </div>
-    */
+  </div>
+)
 
-class Popup {
-  constructor() {
-    this.body = document.getElementById("popup");
-    return;
-  }
 
-  addFilter() {
-    const form = document
-      .createElement("form")
-      .classList.add("row", "align-items-center");
-    const obj = {
-      form: {
-        tag: "form",
-        classes: ["row", "align-items-center"],
-        children: [
-          {
-            tag: "div",
-            classes: "col-3",
-            children: [
-              {
-                tag: "button",
-                classes: ["btn", "btn-primary", "btn-sm"],
-                text: "Submit",
-              },
-            ],
-          },
-          {
-            tag: "div",
-            classes: "col-6",
-            children: [
-              {
-                tag: "input",
-                classes: ["form-control"],
-              },
-            ],
-          },
-        ],
-      },
-    };
-  }
-}
+console.log(document.getElementById('app'))
 
-function domElementGenerate() {}
+const Series = (
+  <div>
+    <form class="row align-items-center">
+      <div class="col-3">
+        <button class="btn btn-primary btn-sm" type="button">Edit</button>
+      </div>
+      <div class="col-6">
+        <input class="form-control" type="text" value="size" disabled />
+      </div>
+      <div class="col-3 form-switch d-flex justify-content-end">
+        <input class="form-check-input" type="checkbox" />
+      </div>
+    </form>
+    <form class="row align-items-center">
+      <div class="col-3">
+        <button class="btn btn-primary btn-sm" type="button">Submit</button>
+      </div>
+      <div class="col-6">
+        <input class="form-control" type="text" value="size" />
+      </div>
+    </form>
+    <div>
+      <button type="button" class="btn btn-primary btn-sm">Add Filter</button>
+    </div>
+  </div>
+)
+
 /**
  * When the popup loads, inject a content script into the active tab,
  * and add a click handler.
  * If we couldn't inject the script, handle the error.
  */
-/*
-browser.tabs
-  .executeScript({ file: "/content_scripts/beastify.js" })
-  .then(listenForClicks)
-  .catch(reportExecuteScriptError);
-*/
+
+document.getElementById('app').replaceWith(App)
