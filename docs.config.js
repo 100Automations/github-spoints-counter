ce95b88f7a4a5e9b8bd8dfbf3777043aea78aaa6
Aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-09-28 18:47:27 -0700

    testing public path

--- docs.config.js
+++ docs.config.js
@@ -43,7 +43,7 @@ module.exports = (env) => {
     output: {
       assetModuleFilename: "assets/[name][ext][query]",
       path: path.resolve(__dirname, "docs"),
-      publicPath: env.development ? "auto" : "github-spoints-counter/",
+      publicPath: env.development ? "auto" : "/",
       clean: {
         keep(asset) {
           return ["index.html"].some((path) => {
