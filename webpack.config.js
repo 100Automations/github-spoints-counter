7d70aeae6478d86ceab3e76afb7ccbc54e67634e
Aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-10-20 14:56:14 -0700

    made firefox dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,52 @@
 const path = require("path");
 
-module.exports = {
-  entry: {
-    index: { import: "./src/index.ts", filename: "[name].bundle.js" },
-    popup: { import: "./src/popup/Popup.tsx", filename: "[name].bundle.js" },
-  },
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.(svg)$/i,
-        type: "asset/inline",
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js", "jsx"],
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    clean: {
-      keep(asset) {
-        return ["popup.html", "manifest.json", "icons/"].some((path) => {
-          return asset.includes(path);
-        });
+module.exports = (env) => {
+  return {
+    entry: {
+      index: { import: "./src/index.ts", filename: "[name].bundle.js" },
+      popup: { import: "./src/popup/Popup.tsx", filename: "[name].bundle.js" },
+    },
+    devtool: "inline-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(svg)$/i,
+          type: "asset/inline",
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js", "jsx"],
+    },
+    output: {
+      path: path.resolve(__dirname, `dist/${env.browser}`),
+      clean: {
+        keep(asset) {
+          return ["popup.html", "manifest.json", "icons/"].some((path) => {
+            return asset.includes(path);
+          });
+        },
       },
     },
-  },
+  };
 };
